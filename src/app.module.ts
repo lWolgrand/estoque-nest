48b784062af7d6953a3e7d4ced300a2278751089
Wolgrand Lopes <lopeswolgrand@gmail.com>
2022-04-28 02:21:46 -0300

    Instalando sequelize e pg, iniciando uma conexão com o banco usando um docker run na porta 5432.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviceModule } from './device/device.module';
-;
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [DeviceModule, DeviceModule],
+  imports: [DeviceModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/device/entities/device.entity.ts
+++ src/device/entities/device.entity.ts
@@ -1,14 +1,13 @@
 export class Device {
-
-    id : number;
-    serialNumber : string;
-    name : string;
-    description : string;
-    createdAt : Date;
-    updatedAt : Date;
-    Status : number;
-    Category : number;
-    // Invoice_In: number;
-    // Invoice_Out: number;
-    Source : number;
+  id: number;
+  serialNumber: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+  Status: number;
+  Category: number;
+  // Invoice_In: number;
+  // Invoice_Out: number;
+  Source: number;
 }
