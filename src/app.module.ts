a1761619d4fc5e740bf9343785ab5cbddf45ea31
Wolgrand Lopes <lopeswolgrand@gmail.com>
2022-04-29 19:24:09 -0300

    Adicionando DeviceController e DeviceService na app.module e editando o campo do sequelize com as credenciais.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviceModule } from './device/device.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { DeviceController } from './device/device.controller';
+import { DeviceService } from './device/device.service';
 
 @Module({
   imports: [
@@ -10,15 +12,15 @@ import { SequelizeModule } from '@nestjs/sequelize';
       dialect: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'device',
+      username: 'root',
+      password: 'root',
+      database: 'devices',
       autoLoadModels: true,
       synchronize: true,
     }),
     DeviceModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, DeviceController],
+  providers: [AppService, DeviceService],
 })
 export class AppModule {}
