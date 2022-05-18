61cb75d770fae8a8d66acd4088117eae15f14d67
Lopes <lopeswolgrand@gmail.com>
2022-05-18 20:08:12 -0300

    configurando ainda sequelize

--- src/models/device.ts
+++ src/models/device.ts
@@ -1,31 +1,87 @@
 'use strict';
-import { Model } from 'sequelize';
-module.exports = (sequelize, DataTypes) => {
-  class Device extends Model {
+
+import { 
+  Model
+ } from 'sequelize';
+
+interface IDevice {
+  id: number;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+  status:string;
+  category: string;
+  invoiceIn: string;
+  invoiceOut: string;
+  source: number;
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class Device extends Model<IDevice> 
+  implements IDevice{
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+     id!: number;
+     name!: string;
+     description!: string;
+     createdAt!: Date;
+     updatedAt!: Date;
+     status!:string;
+     category!: string;
+     invoiceIn!: string;
+     invoiceOut!: string;
+     source!: number;
+
+    static associateStatus(models: any) {
       // define association here
+      Device.belongsToMany(models.Status, {
+        through: 'DeviceStatus',
+      });
     }
-  }
-  Device.init(
-    {
-      serial_number: DataTypes.NUMBER,
-      name: DataTypes.STRING,
-      description: DataTypes.STRING,
-      created_at: DataTypes.NUMBER,
-      updated_at: DataTypes.NUMBER,
-      status: DataTypes.BOOLEAN,
-      category: DataTypes.STRING,
-      source: DataTypes.STRING,
-    },
-    {
+    static associateCategory(models: any) {
+      // define association here
+      Device.belongsToMany(models.Category, {
+        through: 'DeviceCategory',
+      });      
+    }
+    static associateInvoiceIn(models: any) {
+      // define association here
+      Device.belongsToMany(models.InvoiceIn, {
+        through: 'DeviceInvoiceIn',
+      });      
+    }
+    static associateInvoiceOut(models: any) {
+      // define association here
+      Device.belongsToMany(models.InvoiceOut, {
+        through: 'DeviceInvoiceOut',
+      });      
+    }
+    static associateSource(models: any) {
+      // define association here
+      Device.belongsToMany(models.Source,{
+        through: 'DeviceSource', 
+      });
+    } 
+  }   
+    Device.init({
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: { type: DataTypes.STRING, allowNull: false },
+      description: { type: DataTypes.STRING, allowNull: false },
+      createdAt: { type: DataTypes.DATE, allowNull: false },
+      updatedAt: { type: DataTypes.DATE, allowNull: false },
+      status: { type: DataTypes.STRING, allowNull: false },
+      category: { type: DataTypes.STRING, allowNull: false },
+      invoiceIn: { type: DataTypes.STRING, allowNull: false },
+      invoiceOut: { type: DataTypes.STRING, allowNull: false },
+      source: { type: DataTypes.INTEGER, allowNull: false },
+
+    }, {
       sequelize,
-      modelName: 'Device',
-    },
-  );
-  return Device;
-};
+      modelName: 'Status',
+    });
+    return Device;
+  };
