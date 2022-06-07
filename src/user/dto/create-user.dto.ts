9d5707b0d76a25ec794c5781d4dbbb4e307b8b49
Lopes <lopeswolgrand@gmail.com>
2022-06-07 18:17:12 -0300

    Fechando swagger user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
 export class CreateUserDto {
+    /**
+     * Entrada de dados para inserir o nome do usuario.
+     * Campo obrigatorio..
+     * @exemple: "Glauber"
+     */
     username: string;
+    /**
+     * Entrada de dados para inserir o email do usuario.
+     * Campo obrigatorio..
+     * @exemple: "
+     */
     email: string;
+    /**
+     * Entrada de dados para inserir a senha do usuario.
+     * Campo obrigatorio..
+     * @exemple: "123456"
+     */
     password: string;
+    
 }
