6c1cf6eb234b997a476e07e587d7ed0b80fa46e8
Lopes <lopeswolgrand@gmail.com>
2022-06-08 14:40:33 -0300

    Realizando seed

--- database-old/script/new-script.sql
+++ database-old/script/new-script.sql
@@ -1,11 +1,11 @@
 
-insert into "Statuses"(id, name) values 
+insert into "Status"(id, name) values 
 (1, 'Ativo'),
 (2, 'Emprestado'),
 (3, 'Manutenção'),
 (4, 'Offline') ON CONFLICT DO NOTHING;
 
-insert into "Categories"(id, name) values 
+insert into "Category"(id, name) values 
 (1, 'Móveis'),
 (2, 'Notebook'),
 (3, 'Desktop'),
@@ -16,7 +16,7 @@ insert into "Categories"(id, name) values
 (8, 'Chromebook'),
 (9, 'Peça-hardware') ON CONFLICT DO NOTHING;
 
-insert into "Sources"(id, name) values 
+insert into "Source"(id, name) values 
 (1, 'Doação'),
 (2, 'Compra'),
 (3, 'Alugado') ON CONFLICT DO NOTHING;
