ab219b3213af6ef8a3c4b84b9b8bcd1838f11df2
Lopes <lopeswolgrand@gmail.com>
2022-06-08 13:09:18 -0300

    Resolvendo arquivo config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,12 @@
+//config dotenv
+require('dotenv').config(); 
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "device",
-    "host": "localhost",
-    "dialect": "postgres"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   }
 }
