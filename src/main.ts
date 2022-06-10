cb425bbe06ff9baa17252398a4d9840a93d62a3a
Lopes <lopeswolgrand@gmail.com>
2022-06-09 23:36:06 -0300

    Resolvendo cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
   .setTitle('Documentação com Swagger - Estoque Plataforma')
   .setDescription(
@@ -17,7 +17,7 @@ async function bootstrap() {
 
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
-
+    
   await app.listen(process.env.PORT || 3000);
 }
 
